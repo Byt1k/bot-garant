2aeec07c0d3046a2cbca720c455765040a19be5d
Byt1k <byt1kip82@gmail.com>
2022-07-06 19:45:23 +0300

    added catalog and basket pages

--- js/main.js
+++ js/main.js
@@ -43,3 +43,62 @@ const games = {
 
 $('.mobile-games__slider').owlCarousel(games)
 $('.computer-games__slider').owlCarousel(games)
+
+const catalogItems = document.querySelectorAll('.catalog .item')
+
+catalogItems.forEach(i => {
+    const btnAdd = i.querySelector('.btn-add')
+
+    const btnPlus = i.querySelector('.plus');
+    const btnMinus = i.querySelector('.minus');
+    
+    let count = i.querySelector('.count')
+
+    btnAdd.addEventListener('click', (e) => {
+        e.target.parentNode.classList.add('active');
+        count.innerHTML = 1;
+    })
+
+    btnPlus.addEventListener('click', () => {
+        count.innerHTML = Number(count.innerHTML) + 1;
+    })
+
+    btnMinus.addEventListener('click', () => {
+        if (+count.innerHTML > 0) {
+            count.innerHTML = Number(count.innerHTML) - 1;
+        }
+
+        if (+count.innerHTML == 0) {
+            i.querySelector('.bottom').classList.remove('active')
+        }
+    })
+})
+
+
+const basketItems = document.querySelectorAll('.basket .item');
+const basketEmpty = document.querySelector('.basket-empty');
+
+// if (!basketItems.length) {
+//     basketEmpty.style.display = 'block';
+// }
+
+basketItems.forEach(i => {
+    const btnPlus = i.querySelector('.plus');
+    const btnMinus = i.querySelector('.minus');
+
+    let count = i.querySelector('.count')
+
+    btnPlus.addEventListener('click', () => {
+        count.innerHTML = Number(count.innerHTML) + 1;
+    })
+
+    btnMinus.addEventListener('click', () => {
+        if (+count.innerHTML > 0) {
+            count.innerHTML = Number(count.innerHTML) - 1;
+        }
+
+        // if (+count.innerHTML == 0) {
+        //     i.remove();
+        // }
+    })
+})
